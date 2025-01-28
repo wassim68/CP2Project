26e7539934d2c74afd5cde9a66d78d0b9d7af783
wassim68 <sdk.wassim6@gmail.com>
2025-01-28 20:54:45 +0100

    added models (user,company,student -> Auth_app / application,opportunity,team -> post_app )

--- Auth/models.py
+++ Auth/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractUser,BaseUserManager,PermissionsMixin
+from post.models import application,opportunity,team
 
-# Create your models here.
+types = {}
+categories = {}
+skills_list = {}
+
+
+class company(models.Model):
+    category = models.CharField(choices= categories)
+    opportunities = models.models.ForeignKey(opportunity, on_delete=models.CASCADE,related_name = 'opportunies')
+
+class student(models.Model):
+    skills = models.CharField(choices= skills_list)
+    rating = models.IntegerField(default=0)
+    applications = models.ForeignKey(application,on_delete= models.CASCADE,related_name='applications')
+    
+
+class user(AbstractUser,PermissionsMixin):
+    email = models.email_field()
+    number = models.models.IntegerField()
+    created_date = models.DateField(auto_now_add=True)
+    profile_pic = models.ImageField(null = 1)
+    Type = models.CharField( choices= types)
+    companyID = models.OneToOneField(company , on_delete= models.CASCADE,related_name="company", null = 1)
+    studentID = models.OneToOneField(student , on_delete= models.CASCADE,related_name="student", null = 1)
+
