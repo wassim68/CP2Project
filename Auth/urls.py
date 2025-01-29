d05a272b199ebf59be1553208893b498117bf0a0
Moundher bouroumana <156693930+Moundher122@users.noreply.github.com>
2025-01-29 15:32:11 +0100

    there is problem when delete the user model

--- Auth/urls.py
+++ Auth/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('Login',views.Login.as_view()),
     path('Signup',views.Signup.as_view()),
-    path('Refresh',TokenRefreshView.as_view())
+    path('Refresh',TokenRefreshView.as_view()),
+    path('user',views.Deleteacc.as_view())
 ]
