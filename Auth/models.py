1983cf82904ef23888022912d800b080eaee526b
Moundher bouroumana <156693930+Moundher122@users.noreply.github.com>
2025-01-30 11:43:43 +0100

    add forgot password with otp

--- Auth/models.py
+++ Auth/models.py
@@ -29,6 +29,9 @@ class company(models.Model):
 
 class Skills(models.Model):
     name=models.TextField()
+    def __str__(self):
+        return self.name
+    
 
 class Student(models.Model):
     education=models.CharField(max_length=50)
