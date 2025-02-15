7b19ba7c79a56a84664132e8f20f0e3aa0e17b40
Moundher bouroumana <156693930+Moundher122@users.noreply.github.com>
2025-02-15 14:59:12 +0100

    add the docs

--- post/urls.py
+++ post/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('team/',views.team_crud.as_view()),
     path('team/managing/',views.team_managing.as_view()),
     path('application/<int:id>/',views.applications.as_view()),
-    path('application',views.application_crud.as_view())
+    path('application',views.application_crud.as_view()),
+    path('deapplication/<int:id>/',views.deleteapplication.as_view()),
+    path('<int:id>/',views.company_app_management.as_view()),
 ]
