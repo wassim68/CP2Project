7374cf229ea2e2631194be5065356158ad1d9964
Moundher bouroumana <156693930+Moundher122@users.noreply.github.com>
2025-04-24 21:42:50 +0200

    fix the scc

--- ProjectCore/asgi.py
+++ ProjectCore/asgi.py
@@ -3,7 +3,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing  # Replace 'chat' with your app name if different
-
+from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProjectCore.settings")
 
 application = ProtocolTypeRouter({
@@ -14,3 +14,5 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
